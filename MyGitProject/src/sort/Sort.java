067c13f5242a970e028a021aa09171bfe1e1dc28
jaeimveilion <jaeimveilion@172.30.1.33>
2016-11-06 18:39:51 +0900

    Basic Quick Sort Update

--- MyGitProject/src/sort/Sort.java
+++ MyGitProject/src/sort/Sort.java
@@ -1,6 +1,16 @@
 package sort;
 
+import java.util.Random;
+
 public class Sort<T> {
+	
+	public static void Sort(int[] data, int type, int asc_dec) {
+		if(type == Type.QUICK_SORT) {
+			if(asc_dec == Type.ASC) {
+				QuickSortAsc(data, 0, data.length - 1);
+			}
+		}
+	}
 	
 	public void Sort(T[] data, int type) {
 		if(type == Type.QUICK_SORT) {
@@ -8,6 +18,68 @@ public class Sort<T> {
 		}
 	}
 	
-	private void QuickSort(T[] data, int start, int end) {
+	private static void QuickSortAsc(int[] data, int start, int end) {
+		int pivot = end;
+		int left = start;
+		int right = end - 1;
+		
+		if(end - start <= 1) {
+			return;
+		}
+		
+		while(left < right) {
+			while(left < end && data[left] <= data[pivot]) {
+				left++;
+			}
+			while(start < right && data[pivot] <= data[right]) {
+				right--;
+			}
+			if(left < right) {
+				int temp = data[left];
+				data[left] = data[right];
+				data[right] = temp;
+			}
+		}
+		
+		int temp = data[left];
+		data[left] = data[pivot];
+		data[pivot] = temp;
+		
+		QuickSortAsc(data, start, left - 1);
+		QuickSortAsc(data, left + 1, end);
+	}
+	
+	private void QuickSortAsc(T[] data, int start, int end) {
+		int pivot = end;
+		
+		int left = start;
+		int right = end - 1;
+		
+		while(left < right) {
+			
+		}
+	}
+	
+	public static void main(String[] args) {
+		int[] data = new int[20];
+		Random rand = new Random(System.currentTimeMillis());
+		
+		for(int i = 0; i < data.length; i++) {
+			data[i] = rand.nextInt(20);
+		}
+		
+		for(int i = 0; i < data.length; i++) {
+			System.out.print(data[i] +" ");
+		}
+		System.out.println();
+		
+		Sort.Sort(data, Type.QUICK_SORT, Type.ASC);
+		
+		for(int i = 0; i < data.length; i++) {
+			System.out.print(data[i] +" ");
+		}
+		System.out.println();
+		
+		
 	}
 }
--- MyGitProject/src/sort/Type.java
+++ MyGitProject/src/sort/Type.java
@@ -2,4 +2,7 @@ package sort;
 
 public class Type {
 	public static int QUICK_SORT = 0;
+	
+	public static int ASC = -1;
+	public static int DEC = -2;
 }
