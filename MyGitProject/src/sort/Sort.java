9658d539cc7eb7be516604675a2c6c4547dda989
jaeimveilion <jaeimveilion@172.30.1.57>
2016-11-06 17:10:52 +0900

    Quick ing...

---
+++ MyGitProject/src/sort/Sort.java
@@ -0,0 +1,13 @@
+package sort;
+
+public class Sort<T> {
+	
+	public void Sort(T[] data, int type) {
+		if(type == Type.QUICK_SORT) {
+			
+		}
+	}
+	
+	private void QuickSort(T[] data, int start, int end) {
+	}
+}
---
+++ MyGitProject/src/sort/Type.java
@@ -0,0 +1,5 @@
+package sort;
+
+public class Type {
+	public static int QUICK_SORT = 0;
+}
