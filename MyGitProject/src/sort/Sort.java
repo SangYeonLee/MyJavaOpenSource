c801ce29b7dd7f95ba475cb88f4b750406487abf
jaeimveilion <jaeimveilion@172.30.1.57>
2016-11-08 17:44:57 +0900

    -ss option needed

--- MyGitProject/src/sort/Sort.java
+++ MyGitProject/src/sort/Sort.java
@@ -1,85 +1,237 @@
 package sort;
 
+import java.util.Calendar;
 import java.util.Random;
 
-public class Sort<T> {
+public class Sort {
 	
-	public static void Sort(int[] data, int type, int asc_dec) {
-		if(type == Type.QUICK_SORT) {
-			if(asc_dec == Type.ASC) {
-				QuickSortAsc(data, 0, data.length - 1);
+	public static Random rand = new Random(System.currentTimeMillis());
+	
+	public static void SortAlgor(int[] data, int sortType, int type) {
+		if(sortType == Type.QUICK_SORT) {
+			QuickSort(data, 0, data.length - 1, type);			
+		} else if(sortType == Type.INSERT_SORT) {
+			InsertSort(data, 0, data.length - 1, type);
+		}
+	}
+	
+	public static void ThreadQuickSort(int[] data, int type) {
+		ThreadQuickSort tqs = new ThreadQuickSort(data, 0, data.length - 1, type, 0);
+		tqs.start();
+		try {
+			tqs.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void InsertSort(int[] data, int start, int end, int type) {
+		for(int i = start + 1; i <= end; i++) {
+			int key = data[i];
+			int j = i - 1;
+			if(type == Type.ASC) {
+				while(i >= start && data[j]  > key) {
+					data[i + 1] = i;
+					i--;
+				}
+			} else if (type == Type.DEC) {
+				while(i >= start && data[j]  < key) {
+					data[i + 1] = i;
+					i--;
+				}
 			}
+			data[i + 1] = key;
 		}
 	}
-	
-	public void Sort(T[] data, int type) {
-		if(type == Type.QUICK_SORT) {
-			
-		}
-	}
-	
-	private static void QuickSortAsc(int[] data, int start, int end) {
-		int pivot = end;
-		int left = start;
-		int right = end - 1;
+
+	public static void QuickSort(int[] data, int start, int end, int type) {
 		
-		if(end - start <= 1) {
+		if(end - start < 200) {
+			Sort.InsertSort(data, start, end, type);
 			return;
 		}
 		
-		while(left < right) {
-			while(left < end && data[left] <= data[pivot]) {
-				left++;
-			}
-			while(start < right && data[pivot] <= data[right]) {
-				right--;
-			}
-			if(left < right) {
-				int temp = data[left];
-				data[left] = data[right];
-				data[right] = temp;
-			}
-		}
+		int pivot = rand.nextInt(end - start) + start;
 		
-		int temp = data[left];
-		data[left] = data[pivot];
+		int temp = data[end];
+		data[end] = data[pivot];
 		data[pivot] = temp;
 		
-		QuickSortAsc(data, start, left - 1);
-		QuickSortAsc(data, left + 1, end);
-	}
-	
-	private void QuickSortAsc(T[] data, int start, int end) {
-		int pivot = end;
 		
+		pivot = end;
 		int left = start;
 		int right = end - 1;
 		
-		while(left < right) {
-			
+		if(type == Type.ASC) {
+			while(true) {
+				while(left < end && data[left] <= data[pivot]) {
+					left++;
+				}
+				while(start <= right && data[pivot] < data[right]) {
+					right--;
+				}
+				if(left <= right) {
+					temp = data[left];
+					data[left] = data[right];
+					data[right] = temp;
+				} else {
+					temp = data[left];
+					data[left] = data[pivot];
+					data[pivot] = temp;
+					break;
+				}
+			}
+		} else if(type == Type.DEC) {
+			while(true) {
+				while(left < end && data[left] > data[pivot]) {
+					left++;
+				}
+				while(start <= right && data[pivot] >= data[right]) {
+					right--;
+				}
+				if(left <= right) {
+					temp = data[left];
+					data[left] = data[right];
+					data[right] = temp;
+				} else {
+					temp = data[left];
+					data[left] = data[pivot];
+					data[pivot] = temp;
+					break;
+				}
+			}
+		}
+		
+		
+		if(start < left - 1) {
+			QuickSort(data, start, left - 1, type);
+		}
+		if(left + 1 < end) {
+			QuickSort(data, left + 1, end, type);
 		}
 	}
 	
 	public static void main(String[] args) {
-		int[] data = new int[20];
-		Random rand = new Random(System.currentTimeMillis());
-		
+		int[] data = new int[1000000];
+
 		for(int i = 0; i < data.length; i++) {
 			data[i] = rand.nextInt(20);
 		}
+		Calendar startTime = Calendar.getInstance();
+		System.out.println(startTime.getTime());
 		
-		for(int i = 0; i < data.length; i++) {
-			System.out.print(data[i] +" ");
-		}
+		//Sort.SortAlgor(data, Type.QUICK_SORT, Type.ASC);
+		Sort.ThreadQuickSort(data, Type.ASC);
+		Calendar endTime = Calendar.getInstance();
+		System.out.println(endTime.getTime());
+		long dif = endTime.getTimeInMillis() - startTime.getTimeInMillis();
+		dif /= 1000;
+		long sec = dif % 60;
+		long min = (dif / 60) % 60;
+		long hour = (dif / 60) / 60;
+		System.out.println(hour+":"+min+":"+sec);
+		
+		//Sort.SortAlgor(data, Type.QUICK_SORT, Type.DEC);
+		//Sort.ThreadQuickSort(data, Type.DEC);
+		
 		System.out.println();
-		
-		Sort.Sort(data, Type.QUICK_SORT, Type.ASC);
-		
-		for(int i = 0; i < data.length; i++) {
-			System.out.print(data[i] +" ");
+	}
+}
+class ThreadQuickSort extends Thread {
+	int[] data;
+	int start;
+	int end;
+	int type;
+	int depth;
+	public ThreadQuickSort(int[] data, int start, int end, int type, int depth) {
+		this.data = data;
+		this.start = start;
+		this.end = end;
+		this.type = type;
+		this.depth = depth;
+	}
+	public void run() {
+		if(end - start < 200) {
+			Sort.InsertSort(data, start, end, type);
+			return;
 		}
-		System.out.println();
 		
+		int pivot = Sort.rand.nextInt(end - start) + start;
 		
+		int temp = data[end];
+		data[end] = data[pivot];
+		data[pivot] = temp;
+		
+		pivot = end;
+		int left = start;
+		int right = end - 1;
+		
+		if(type == Type.ASC) {
+			while(true) {
+				while(left < end && data[left] <= data[pivot]) {
+					left++;
+				}
+				while(start <= right && data[pivot] < data[right]) {
+					right--;
+				}
+				if(left <= right) {
+					temp = data[left];
+					data[left] = data[right];
+					data[right] = temp;
+				} else {
+					temp = data[left];
+					data[left] = data[pivot];
+					data[pivot] = temp;
+					break;
+				}
+			}
+			
+		} else if (type == Type.DEC) {
+			while(true) {
+				while(left < end && data[left] > data[pivot]) {
+					left++;
+				}
+				while(start <= right && data[pivot] >= data[right]) {
+					right--;
+				}
+				if(left <= right) {
+					temp = data[left];
+					data[left] = data[right];
+					data[right] = temp;
+				} else {
+					temp = data[left];
+					data[left] = data[pivot];
+					data[pivot] = temp;
+					break;
+				}
+			}
+		} else {
+			System.err.println("Option Key is wrong!!");
+			return;
+		}
+		
+		if (depth < 9) {
+			ThreadQuickSort tqs1 = new ThreadQuickSort(data, start, left-1, this.type, this.depth + 1);
+			ThreadQuickSort tqs2 = new ThreadQuickSort(data, left + 1, end, this.type, this. depth + 1);
+		
+			if(start < left - 1) {
+				tqs1.start();
+			}
+			if(left + 1 < end) {
+				tqs2.start();
+			}
+		
+			try {
+				tqs1.join();
+				tqs2.join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			Sort.QuickSort(data, start, left - 1, type);
+			Sort.QuickSort(data, left + 1, end, type);
+		}
 	}
 }
--- MyGitProject/src/sort/Type.java
+++ MyGitProject/src/sort/Type.java
@@ -2,6 +2,7 @@ package sort;
 
 public class Type {
 	public static int QUICK_SORT = 0;
+	public static int INSERT_SORT = 1;
 	
 	public static int ASC = -1;
 	public static int DEC = -2;
